b5f19ff15663251522c2e02cf37ef5c27c94b788
unknows14574 <131650796+unknows14574@users.noreply.github.com>
2023-05-06 12:01:22 +0200

    Add files via upload

---
+++ Nebula_Paintball/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games {'gta5'}
+author 'Marvin_Selbonne & LOAF'
+description 'Paintball Game'
+version '1.0.0'
+
+shared_scripts { "shared/*.lua" }
+
+client_scripts {
+    "client/*.lua",
+}
+
+server_scripts {
+    "server/*.lua",
+}
